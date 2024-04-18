33b6ab421f474b90b7986e76dc5ce303a5022c6e
jhoncarter7 <rainikhil044@gmail.com>
2024-04-18 15:28:35 +0530

    Add TaskInput component for managing todo list

---
+++ src/components/TaskInput.jsx
@@ -0,0 +1,74 @@
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
+import Typography from "@mui/material/Typography";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useState } from "react";
+
+
+export default function TaskInput() {
+  const [todo, setTodo] = useState("");
+  const matches = useMediaQuery("(min-width:600px)");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(todo);
+    setTodo("");
+  };
+
+  return (
+    <Box
+      sx={{
+        marginTop: 2,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Box sx={{ display: "flex", alignItems: "center" }}>
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <FormatListBulletedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          List of Todo
+        </Typography>
+      </Box>
+      <Box
+        component="form"
+        onSubmit={handleSubmit}
+        noValidate
+        sx={{
+          mt: 1,
+          display: "flex",
+          gap: matches ? "1rem" : "",
+          flexDirection: matches ? "row" : "column",
+        }}
+      >
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="Text"
+          label="Type your todo here"
+          name="Todo"
+          top="2px"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+          sx={
+            matches
+              ? { width: "30vw", backgroundColor: "white" }
+              : { width: "60vw", backgroundColor: "white" }
+          }
+        />
+        <Button
+          type="submit"
+          variant="contained"
+          sx={matches ? { mt: 3, mb: 2 } : { mb: 1 }}
+        >
+          Add
+        </Button>
+      </Box>
+    </Box>
+  );
+}
